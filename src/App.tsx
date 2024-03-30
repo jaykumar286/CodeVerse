061c094b4746a1f35d32a460bcb34aa27c8365ff
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-03-30 14:31:58 +0530

    added support html on the markdown rendering

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,19 @@ import * as ace from "ace-builds/src-noconflict/ace";
 
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import SampleProlbem1 from "./constants/SampleProlbem1";
 import ProblemDescription from "./pages/ProblemDescription/ProblemDescription";
 ace.config.set("basePath", "/assets/ui/");
 ace.config.set("modePath", "");
 ace.config.set("themePath", "");
 
 function App() {
-  const markdownText = `
-  #  Welcome to StackEdit!
-  
-  ![image](https://assets.leetcode.com/uploads/2018/10/12/knight.png)
 
-  Hi! I'm your first Markdown file in **StackEdit**. If you want to learn about StackEdit, you can read me. If you want to play with Markdown, you can edit me. Once you have finished with me, you can create new files by opening the **file explorer** on the left corner of the navigation bar.
-  `;
   return (
     <>
       <Navbar />
       <Sidebar />
-      <ProblemDescription descriptionText={markdownText} />
+      <ProblemDescription descriptionText={SampleProlbem1.problemStatement} />
     </>
   );
 }
--- src/pages/ProblemDescription/ProblemDescription.tsx
+++ src/pages/ProblemDescription/ProblemDescription.tsx
@@ -1,106 +1,120 @@
 /* eslint-disable simple-import-sort/imports */
 
-import { useState } from 'react';
-import AceEditor from 'react-ace';
-import ReactMarkdown from 'react-markdown';
+import { useState } from "react";
+import AceEditor from "react-ace";
+import ReactMarkdown from "react-markdown";
 
 import "ace-builds/src-noconflict/theme-monokai";
-import 'ace-builds/src-noconflict/ace';
+import "ace-builds/src-noconflict/ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/mode-c_cpp";
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/ext-language_tools";
+import DOMPurify from 'dompurify';
+import rehypeRaw from 'rehype-raw';
+function Description({ descriptionText }: { descriptionText: string }) {
+  const sanitizedMarkdown = descriptionText;
 
-function Description({ descriptionText }: {descriptionText: string}) {
+  const [activeTab, setActiveTab] = useState("statement");
+  const [leftWidth, setLeftWidth] = useState(50);
+  const [isDragging, setIsDragging] = useState(false);
 
+  const startDragging = (e: { preventDefault: () => void; }) => {
+    setIsDragging(true);
+    e.preventDefault();
+  };
 
-    const sanitizedMarkdown = descriptionText;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const stopDragging = () => {
+    if (isDragging) {
+      setIsDragging(false);
+    }
+  };
 
+  const onDrag = (e: { clientX: number }) => {
+    if (!isDragging) return;
 
-    const [activeTab, setActiveTab] = useState('statement');
-    const [leftWidth, setLeftWidth] = useState(50);
-    const [isDragging, setIsDragging] = useState(false);
+    const newLeftWidth = (e.clientX / window.innerWidth) * 100;
+    if (newLeftWidth > 10 && newLeftWidth < 90) {
+      setLeftWidth(newLeftWidth);
+    }
+  };
 
-    const startDragging = (e: MouseEvent) => {
-        setIsDragging(true);
-        e.preventDefault();
-    };
+  const isActiveTab = (tabName: string) => {
+    if (activeTab === tabName) {
+      return "tab tab-active";
+    } else {
+      return "tab";
+    }
+  };
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const stopDragging = () => {
-        if(isDragging) {
-            setIsDragging(false);
-        }
-    };
-
-    const onDrag = (e: { clientX: number; }) => {
-        if(!isDragging) return;
-        
-        const newLeftWidth = (e.clientX / window.innerWidth) * 100;
-        if(newLeftWidth > 10 && newLeftWidth < 90) {
-            setLeftWidth(newLeftWidth);
-        }
-
-    };
-
-    const isActiveTab = (tabName: string) => {
-        if(activeTab === tabName) {
-            return 'tab tab-active';
-        } else {
-            return 'tab';
-        }
-    };
-
-
-
-    return (
-        <div 
-            className='flex w-screen h-screen'
-            onMouseMove={onDrag}
-            onMouseUp={stopDragging}
-            
-        >
-
-            <div className='leftPanel h-full overflow-auto' style={{ width: `${leftWidth}%`}}>
-
-                <div role="tablist" className="tabs tabs-boxed w-3/5">
-                    <a onClick={() => setActiveTab('statement')} role="tab" className={isActiveTab("statement")}>Problem Statement</a>
-                    <a onClick={() => setActiveTab('editorial')} role="tab" className={isActiveTab("editorial")}>Editorial</a>
-                    <a onClick={() => setActiveTab('submissions')} role="tab" className={isActiveTab("submissions")}>Submissions</a>
-                </div>
-
-                <div className='markdownViewer p-[20px] basis-1/2'>
-                    <ReactMarkdown>
-                        {sanitizedMarkdown}
-                    </ReactMarkdown>
-                </div>
-
-
-            </div>
-
-            <div className='divider cursor-col-resize w-[5px] bg-slate-200 h-full' onMouseDown={startDragging}></div>
-
-            <div className='rightPanel h-full overflow-auto' style={{ width: `${100-leftWidth}%`}}>
-                <div className='editorContainer'>
-                    <AceEditor
-                        mode='javascript'
-                        theme='monokai'
-                        name='codeEditor'
-                        className='editor'
-                        style={{ width: '100%'}}
-                        setOptions={{
-                            enableBasicAutocompletion: true,
-                            enableLiveAutocompletion: true,
-                            showLineNumbers: true,
-                            fontSize: 16
-                        }}
-                    />
-                </div>
-            </div>
-
+  return (
+    <div
+      className="flex w-screen h-screen"
+      onMouseMove={onDrag}
+      onMouseUp={stopDragging}
+    >
+      <div
+        className="leftPanel h-full overflow-auto"
+        style={{ width: `${leftWidth}%` }}
+      >
+        <div role="tablist" className="tabs tabs-boxed w-3/5">
+          <a
+            onClick={() => setActiveTab("statement")}
+            role="tab"
+            className={isActiveTab("statement")}
+          >
+            Problem Statement
+          </a>
+          <a
+            onClick={() => setActiveTab("editorial")}
+            role="tab"
+            className={isActiveTab("editorial")}
+          >
+            Editorial
+          </a>
+          <a
+            onClick={() => setActiveTab("submissions")}
+            role="tab"
+            className={isActiveTab("submissions")}
+          >
+            Submissions
+          </a>
         </div>
-    );
+
+        <div className="markdownViewer p-[20px] basis-1/2">
+          <ReactMarkdown rehypePlugins={[rehypeRaw]}>{sanitizedMarkdown}</ReactMarkdown>
+        </div>
+      </div>
+
+      <div
+        className="divider cursor-col-resize w-[5px] bg-slate-200 h-full"
+        onMouseDown={startDragging}
+      ></div>
+
+      <div
+        className="rightPanel h-full overflow-auto"
+        style={{ width: `${100 - leftWidth}%` }}
+      >
+        <div className="editorContainer">
+          <AceEditor
+            mode="javascript"
+            theme="monokai"
+            name="codeEditor"
+            className="editor"
+            style={{ width: "100%" }}
+            setOptions={{
+              enableBasicAutocompletion: true,
+              enableLiveAutocompletion: true,
+              showLineNumbers: true,
+              fontSize: 16,
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Description;
